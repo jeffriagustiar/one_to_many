1104fb6bc1e1735468510c7ca6b7dc789bee5fe5
jeffriagustiar <jeffriagustiar@gmail.com>
2022-09-01 21:07:06 +0700

    middleware

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,6 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+Route::group(['middleware' => ['admin']], function () {
+    Route::get('/test', function (){ return "a";} );
+});
