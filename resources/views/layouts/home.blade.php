a3e2b29d66be18bd1121c0cf4ab0e3a6674f17ee
Jeffri AGustiar <jeffriagustiar@gmail.com>
2022-09-01 14:06:37 -0700

    slincing

---
+++ resources/views/layouts/home.blade.php
@@ -0,0 +1,34 @@
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>@yield('title')</title>
+        <link href="https://cdn.jsdelivr.net/npm/simple-datatables@latest/dist/style.css" rel="stylesheet" />
+        <link href="{{ url('/assets/css/styles.css') }}" rel="stylesheet" />
+        <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
+    </head>
+    <body class="sb-nav-fixed">
+
+        @include('include.topbar')
+
+        <div id="layoutSidenav">
+
+
+            @include('include.sidenav')
+
+
+            <div id="layoutSidenav_content">
+                @yield('content')
+                @include('include.footer')
+            </div>
+        </div>
+        @stack('before-script')
+        @include('include.script')
+        @stack('after-script')
+    </body>
+</html>
